03e3e638215d37dc78511d7a80cacc656a44045c
Kid Programmer <98392401+Kidprogramer1039@users.noreply.github.com>
2025-01-19 03:23:40 +0900

    feat: domain 추가

---
+++ src/main/java/com/gdg/illum/BusinessDistrict/domain/DistrictPopulationInformation.java
@@ -0,0 +1,27 @@
+package com.gdg.illum.BusinessDistrict.domain;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class DistrictPopulationInformation {
+
+    @SerializedName("year")
+    private final String year; // 데이터 연도
+
+    @SerializedName("region_code")
+    private final String regionCode; // SIGNGU_CD
+
+    @SerializedName("region_name")
+    private final String regionName; // 지역명
+
+    @SerializedName("total_population")
+    private final int totalPopulation; // 총 인구수
+
+    @SerializedName("average_income")
+    private final int averageIncome; // 평균 소득
+}
