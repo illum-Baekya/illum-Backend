1a339d43b9e450687a809cc6d2412afd9ea16d3c
Park, seun <seunpark04@gmail.com>
2025-01-18 18:32:15 +0900

    Initial setting

---
+++ src/main/java/com/gdg/illum/IllumApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.illum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IllumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdg/illum/IllumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.illum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
