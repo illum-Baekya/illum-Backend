4dd71563cd82ded9736fa6a7cedc937128b69d8c
Joon Kim <kjoon418@naver.com>
2025-01-19 02:23:04 +0900

    chore: 클래스 이름 수정

--- src/test/java/com/gdg/illum/jun/store/StoreAmountServiceTest.java
+++ src/test/java/com/gdg/illum/jun/store/StoreAmountServiceTest.java
@@ -1,21 +1,21 @@
 package com.gdg.illum.jun.store;
 
 import com.gdg.illum.BusinessDistrict.domain.StoreType;
-import com.gdg.illum.BusinessDistrict.service.StoreListService;
+import com.gdg.illum.BusinessDistrict.service.StoreAmountService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StoreListServiceTest {
+class StoreAmountServiceTest {
 
     @Autowired
-    StoreListService storeListService;
+    StoreAmountService storeAmountService;
 
     @Test
     void getTotalAmount() {
-        Integer result = storeListService.getTotalAmountOfStoreByCode("41281", StoreType.ART_SPORTS);
+        Integer result = storeAmountService.getTotalAmountOfStoreByCode("41281", StoreType.ART_SPORTS);
 
         Assertions.assertThat(result).isNotNull();
         System.out.println(result);
